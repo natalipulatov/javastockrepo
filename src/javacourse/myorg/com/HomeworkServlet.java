21a1764c4ce09f6d20090048935d6abc8a7bffd1
natali <natalipulatov@gmail.com>
2015-03-02 23:11:35 +0200

    first version

---
+++ src/javacourse/myorg/com/HomeworkServlet.java
@@ -0,0 +1,11 @@
+package javacourse.myorg.com;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HomeworkServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
