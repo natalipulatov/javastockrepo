c70f7dfa138359ce1f0e38bff04b5d9826341485
natali <natalipulatov@gmail.com>
2015-04-02 22:24:10 +0300

    Exercise-2.0

--- src/javacourse/myorg/com/HomeworkServlet.java
+++ src/javacourse/myorg/com/HomeworkServlet.java
@@ -1,11 +1,15 @@
 package javacourse.myorg.com;
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class HomeworkServlet extends HttpServlet {
+	int num1 = 4, num2 = 3, num3 = 7;
+	int result =(num1+num2)*num3;
+	String resultstr = new String ( "<h1> Result of ("+num1+"+"+num2+")"+"*"+num3+"="+result+"</h1>");
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		resp.getWriter().println(resultstr);
 	}
 }
